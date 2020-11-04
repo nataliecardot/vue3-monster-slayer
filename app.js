29c26d6791a8c805fdb0b17edfa056355a9762a0
Natalie-Cardot <nataliecardot@gmail.com>
2020-11-03 19:26:17 -0800

    Add initial data and methods

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      // Random number between 5 and 12
+      const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
+      this.monsterHealth -= attackValue;
+    },
+    attackPlayer() {
+      const attackValue = Math.floor(Math.random() * (15 - 8)) + 8;
+      this.playerHealth -= attackValue;
+    },
+  },
+});
 
 app.mount('#game');
