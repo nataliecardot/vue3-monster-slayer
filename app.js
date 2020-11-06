ca0f98acbd9e1095aa396b0cf2a723281df1549e
Natalie-Cardot <nataliecardot@gmail.com>
2020-11-05 19:44:51 -0800

    Fix syntax

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ const app = Vue.createApp({
   },
   methods: {
     startGame() {
-      this.playerHealth = 100,
-      this.monsterHealth = 100,
-      this.currentRound = 0,
-      this.winner = null,
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
     },
     attackMonster() {
       this.currentRound++;
@@ -83,8 +83,8 @@ const app = Vue.createApp({
       this.attackPlayer();
     },
     surrender() {
-      this.winner = 'monster'
-    }
+      this.winner = 'monster';
+    },
   },
 });
 
