872f58cadb7bb851527526f599dd6cccaf937771
Natalie-Cardot <nataliecardot@gmail.com>
2020-11-05 19:39:36 -0800

    Add button to start new game

--- app.js
+++ app.js
@@ -50,6 +50,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100,
+      this.monsterHealth = 100,
+      this.currentRound = 0,
+      this.winner = null,
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
